de2e1910adbd26a56d6325af8b74d527d851a3b1
GundaVenkataNagaSaiRam <117891504+GundaVenkataNagaSaiRam@users.noreply.github.com>
2022-11-24 16:26:27 +0530

    Add files via upload

--- CastleDependency-main/CastleWindsorTest/CastleWindsorTest/Program.cs
+++ CastleDependency-main/CastleWindsorTest/CastleWindsorTest/Program.cs
@@ -9,6 +9,22 @@ namespace CastleWindsorTest
     {
         static void Main(string[] args)
         {
+            var container = RegisterComponents();
+
+            var business = container.Resolve<IHigherBusiness>();
+            business.DoSomething();
+
+            var normalBusiness = container.Resolve<ISomeBusiness>();
+            normalBusiness.DoSomething();
+
+            var logger = container.Resolve<ILogger>();
+            logger.Log("Log Test");
+            logger.Log("Test Complete");
+            Console.ReadKey();
+        }
+
+        private static Castle.Windsor.WindsorContainer RegisterComponents()
+        {
             var container = new Castle.Windsor.WindsorContainer();
 
             container.Register(
@@ -30,16 +46,7 @@ namespace CastleWindsorTest
                 );
             container.Register(Component.For<ILogger>().ImplementedBy<SimpleLogger>());
 
-            var business = container.Resolve<IHigherBusiness>();
-            business.DoSomething();
-
-            var normalBusiness = container.Resolve<ISomeBusiness>();
-            normalBusiness.DoSomething();
-
-            var logger = container.Resolve<ILogger>();
-            logger.Log("Log Test");
-            logger.Log("Test Complete");
-            Console.ReadKey();
+            return container;
         }
     }
 }
